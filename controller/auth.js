12743b97ed226ad01223c69dcece077473276dcc
SAURAV DHAKA <sauravdhaka456@gmail.com>
2023-09-06 10:19:46 +0530

    part-5

--- controller/auth.js
+++ controller/auth.js
@@ -1,30 +1,44 @@
 const { User } = require("../model/User");
+const crypto = require("crypto");
+const { sanitizeUser } = require("../services/common");
+const jwt = require('jsonwebtoken')
+
+const SECRET_KEY = 'SECRET_KEY';
+
 
 exports.createUser = async (req, res) => {
-  const user = new User(req.body);
-
   try {
-    const doc = await user.save();
-    res.status(201).json({id:doc.id,role:doc.role});
+    const salt = crypto.randomBytes(16);
+    crypto.pbkdf2(
+      req.body.password,
+      salt,
+      310000,
+      32,
+      "sha256",
+      async function (err, hashedPassword) {
+        const user = new User({ ...req.body, password: hashedPassword ,salt });
+        const doc = await user.save();
+        req.login(sanitizeUser(doc),(err)=>{ // this is also calls serializer and adds to session
+          if(err){
+            res.status(400).json(err);
+          }
+          else{
+            const token = jwt.sign(sanitizeUser(doc),SECRET_KEY);
+
+            res.status(201).json(token);
+          }
+        })
+      }
+    );
   } catch (err) {
     res.status(400).json(err);
   }
 };
 
 exports.loginUser = async (req, res) => {
-  try {
-    const user = await User.findOne(
-      { email: req.body.email }
-    ).exec();
-    console.log({ user });
-    if (!user) {
-      res.status(401).json({ message: "no such user email" });
-    } else if (user.password === req.body.password) {
-      res.status(201).json({id:user.id , role:user.role});
-    } else {
-      res.status(401).json({ message: "invalid credentials" });
-    }
-  } catch (err) {
-    res.status(400).json(err);
-  }
+  res.json(req.user);
+};
+
+exports.checkUser = async (req, res) => {
+  res.json({status:'success',user:req.user});
 };
--- index.js
+++ index.js
@@ -10,20 +10,125 @@ const authRouter = require('./routes/Auth')
 const cartRouter = require('./routes/Cart')
 const ordersRouter = require('./routes/Order')
 const cors = require('cors')
+const session = require('express-session')
+const passport = require('passport');
+const { User } = require('./model/User');
+const LocalStrategy = require('passport-local').Strategy;
+const jwt = require('jsonwebtoken')
+const JwtStrategy  = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const crypto = require('crypto');
+const { isAuth, sanitizeUser } = require('./services/common');
+
+const SECRET_KEY = 'SECRET_KEY';
+// JWT opitions
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = SECRET_KEY // TODO : should not be in code
+
 
 
 // middlewares
+server.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false, // don't save session if unmodified
+      saveUninitialized: false, // don't create session until something stored
+    })
+  );
+
+server.use(passport.authenticate('session'))
+
 server.use(cors({
     exposedHeaders:['X-Total-Count']
 }))
 server.use(express.json());
-server.use('/products',productRouters.router)
-server.use('/categories',categoryRouter.router)
-server.use('/brands',brandsRouter.router)
-server.use('/users',usersRouter.router)
+server.use('/products',isAuth(),productRouters.router) // we can also use jwt token for client-only auth
+server.use('/categories',isAuth(),categoryRouter.router)
+server.use('/brands',isAuth(),brandsRouter.router)
+server.use('/users',isAuth(),usersRouter.router)
 server.use('/auth',authRouter.router)
-server.use('/cart',cartRouter.router)
-server.use('/orders',ordersRouter.router)
+server.use('/cart',isAuth(),cartRouter.router)
+server.use('/orders',isAuth(),ordersRouter.router)
+
+
+//Passport Strategies
+passport.use(
+    'local',
+    new LocalStrategy(async function (username,password,done){
+      try {
+        const user = await User.findOne(
+          { email: username }
+        ).exec();
+        if (!user) {
+          done(null,false,{ message: "invalid credentials"})
+        }
+        crypto.pbkdf2(
+         password,
+          user.salt,
+          310000,
+          32,
+          "sha256",
+          async function (err, hashedPassword) {
+
+             if (!crypto.timingSafeEqual(user.password,hashedPassword)) {
+              return done(null,false,{ message: "invalid credentials" })
+
+               
+            } 
+            const token = jwt.sign(sanitizeUser(user),SECRET_KEY);
+
+            done(null,token); // this line send to serialize
+            
+          })
+
+      } catch (err) {
+        done(err)
+      }
+    })
+  );
+  
+
+  passport.use(
+    'jwt',
+    new JwtStrategy(opts, async function (jwt_payload, done) {
+      console.log({jwt_payload})
+      try {
+        const user = await User.findOne({id:jwt_payload.sub})
+        if(user){
+          return done(null,sanitizeUser(user));// This calls the serializer
+        }
+        else{
+          return done(null,false)
+        }
+
+      } catch (err) {
+        return done(err,false);
+
+      }
+      
+           
+      
+    })
+  );
+
+// this creates session variable req.user on being called from callbacks
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+      console.log('serialized',user)
+      return cb(null, { id: user.id, role: user.role });
+    });
+  });
+  
+  // this changes session variable req.user when called from authorized request
+  
+  passport.deserializeUser(function (user, cb) {
+    console.log('deserialized',user)
+    process.nextTick(function () {
+      return cb(null, user);
+    });
+  });
+
 
 main().catch(err=>console.log(err))
 
@@ -32,9 +137,6 @@ async function main(){
     console.log('database connected')
 }
 
-server.get('/',(req,res)=>{
-    res.json({status:"success"})
-})
 
 
 server.listen(8080,()=>{
