ed0830db0fef699c2c74c3a37b43579c0d4a69e7
SAURAV DHAKA <sauravdhaka456@gmail.com>
2023-09-07 14:47:06 +0530

    inedx

--- index.js
+++ index.js
@@ -92,7 +92,10 @@ server.use("/users", isAuth(), usersRouter.router);
 server.use("/auth", authRouter.router);
 server.use("/cart", isAuth(), cartRouter.router);
 server.use("/orders", isAuth(), ordersRouter.router);
-
+// this line we add to make react router work in case of other routes doesnt match
+server.get('*', (req, res) =>
+  res.sendFile(path.resolve('build', 'index.html'))
+);
 //Passport Strategies
 passport.use(
   "local",
