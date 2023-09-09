9d1fe97a7b4831c71be61878511a6e942b19c2e5
SAURAV DHAKA <sauravdhaka456@gmail.com>
2023-09-09 18:22:13 +0530

    cart and product shema update

--- model/Cart.js
+++ model/Cart.js
@@ -7,6 +7,11 @@ const cartSchema = new Schema({
     quantity : {type : Number ,required:true},
     product : {type : Schema.Types.ObjectId ,ref:'Product' ,required:true},
     user : {type : Schema.Types.ObjectId ,ref:'User' ,required:true},
+    size : {type : Schema.Types.Mixed },
+    color : {type :Schema.Types.Mixed},
+
+
+
 
 
 })
